63634ad2c033a2e820437be6e61110bfd0ccddd9
mwita <63615394+mwitamatiko@users.noreply.github.com>
2023-03-05 12:37:55 +0300

    Update GitTestController.java

--- src/main/java/com/example/gittest/GitTestController.java
+++ src/main/java/com/example/gittest/GitTestController.java
@@ -3,8 +3,10 @@ package com.example.gittest;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/git")
 public class GitTestController {
 
+    @GET("/")
     public String gitTest(){
         return "test git";
     }
