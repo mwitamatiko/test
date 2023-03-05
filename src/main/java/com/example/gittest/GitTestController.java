57919bc92c141fbd725d623893db3b3f92457636
mwitamatiko <mwitaigayi@gmail.com>
2023-03-05 12:41:14 +0300

    changed the @GetMapping

--- src/main/java/com/example/gittest/GitTestController.java
+++ src/main/java/com/example/gittest/GitTestController.java
@@ -1,12 +1,14 @@
 package com.example.gittest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/git")
 public class GitTestController {
 
-    @GET("/")
+    @GetMapping("/")
     public String gitTest(){
         return "test git";
     }
